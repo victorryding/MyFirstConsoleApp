24f665f680c644653f808b2a75ee200b5e05ff96
Rydingapps <rydingapps@gmail.com>
2019-08-02 12:39:49 +0200

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = true;
+            while (t)
+            {
+                try
+                {
+                    Console.WriteLine("Hej med dig!");
+                    Console.Write("Put et tal ind her:");
+                    string val = Console.ReadLine();
+                    int a = Convert.ToInt32(val);
+                    Console.WriteLine("Du puttede {0} ind!", a);
+                    t = false;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Du skal altså putte et tal ind!! Prøv igen");
+                }
+            }
+        }
+    }
+}
